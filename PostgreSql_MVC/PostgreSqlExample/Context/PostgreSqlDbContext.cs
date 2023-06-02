00a87116dfbeff4520939cbbf384eee801e0596d
Mehmet Kamil Torunoğlu <98940977+torunoglukamil@users.noreply.github.com>
2023-06-02 13:38:03 +0300

    PostgreSql_MVC

---
+++ PostgreSql_MVC/PostgreSqlExample/Context/PostgreSqlDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PostgreSqlExample.Models;
+
+namespace PostgreSqlExample.Context
+{
+    public class PostgreSqlDbContext : DbContext
+    {
+        public PostgreSqlDbContext(DbContextOptions<PostgreSqlDbContext> options) : base(options) {}
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ PostgreSql_MVC/PostgreSqlExample/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PostgreSqlExample.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string FullName => $"{FirstName} {LastName}";
+        public string Email { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public string Gender { get; set; } = null!;
+    }
+}
